bc161c55788b79498ac99483b501e2a1555f966e
Naresh Chaurasia <johndoe@example.com>
2025-06-01 09:18:58 +0530

    Macbook Pro, Checkin Timestamp::Sun Jun  1 09:18:58 IST 2025

---
+++ src/main/java/com/se/helper/DemoHelper.java
@@ -0,0 +1,16 @@
+package com.se.helper;
+
+public class DemoHelper {
+
+    /**
+     * To slow down the execution
+     * For demo purposes only!
+     */
+    public static void pause() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/com/m1/Test5_ClickingTest.java
@@ -0,0 +1,50 @@
+package com.m1;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import com.se.helper.DriverFactory;
+
+public class Test5_ClickingTest {
+
+    private static final String PREFIX = "file:///" + System.getProperty("user.dir") + "/web/";
+
+    @Test
+    public void clickingTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get(PREFIX + "index.html");
+
+        WebElement first = driver.findElement(By.id("firstName"));
+        WebElement checkbox = driver.findElement(By.id("heard-about"));
+        WebElement registerBtn = driver.findElement(By.id("register"));
+
+        checkbox.click();
+        registerBtn.click();
+        DemoHelper.pause();
+
+        driver.quit();
+    }
+
+    @Test
+    public void moreClicking() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get(PREFIX + "index.html");
+
+        WebElement checkbox = driver.findElement(By.id("heard-about"));
+        Actions actions = new Actions(driver);
+
+        actions.doubleClick(checkbox).perform();
+        actions.contextClick(checkbox).perform();
+
+        DemoHelper.pause();
+        driver.quit();
+    }
+}
